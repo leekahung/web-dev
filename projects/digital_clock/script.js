55f4ef9e39061348a5b688f6d8eca32018dcb8dd
Ka Hung Lee <lee.kahung414@gmail.com>
2022-08-27 19:19:45 -0700

    Editig syntax for digital clock script

--- projects/digital_clock/script.js
+++ projects/digital_clock/script.js
@@ -1,42 +1,36 @@
+const yourTime = document.getElementById("time");
+const amTime = document.getElementById("am");
+const pmTime = document.getElementById("pm");
+const yourTZ = document.getElementById("localTZ");
+
 function getTime() {
   let date = Date();
   let localTZ = date.match(/\([a-zA-Z\s]+/g)[0].slice(1);
   let time = date.match(/\d{2}:\d{2}:\d{2}/g)[0];
   let hours = time.slice(0, 2);
   if (hours[0] === "0") {
-    return (
-      document.getElementById("time").innerHTML = time,
-      document.getElementById("am").style.opacity = 1,
-      document.getElementById("localTZ").innerHTML = localTZ
-    );
+      yourTime.innerHTML = time;
+      amTime.style.opacity = 1;
+      yourTZ.innerHTML = localTZ;
   } else {
     if (Number(hours) < 12) {
-      return (
-        document.getElementById("time").innerHTML = time,
-        document.getElementById("am").style.opacity = 1,
-        document.getElementById("localTZ").innerHTML = localTZ
-      );
+        yourTime.innerHTML = time;
+        amTime.style.opacity = 1;
+        yourTZ.innerHTML = localTZ;
     } else if (Number(hours) == 12) {
-      return (
-        document.getElementById("time").innerHTML = time,
-        document.getElementById("pm").style.opacity = 1,
-        document.getElementById("localTZ").innerHTML = localTZ
-      );
+        yourTime.innerHTML = time;
+        pmTime.style.opacity = 1;
+        yourTZ.innerHTML = localTZ;
     } else if (Number(hours) < 22) {
-      return (
-        document.getElementById("time").innerHTML = "0" + String(hours - 12) + time.slice(2),
-        document.getElementById("pm").style.opacity = 1,
-        document.getElementById("localTZ").innerHTML = localTZ
-      )
+        yourTime.innerHTML = "0" + String(hours - 12) + time.slice(2);
+        pmTime.style.opacity = 1;
+        yourTZ.innerHTML = localTZ;
     } else {
-      return (
-        document.getElementById("time").innerHTML = String(hours - 12) + time.slice(2),
-        document.getElementById("pm").style.opacity = 1,
-        document.getElementById("localTZ").innerHTML = localTZ
-      ) 
+        yourTime.innerHTML = String(hours - 12) + time.slice(2);
+        pmTime.style.opacity = 1;
+        yourTZ.innerHTML = localTZ;
     }
   }
 }
-
 
 setInterval(getTime, 1000);
