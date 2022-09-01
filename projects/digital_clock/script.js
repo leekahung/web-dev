564a0a74f51a02cc28eff45ca73bc0f10ed7d98e
Ka Hung Lee <lee.kahung414@gmail.com>
2022-09-01 00:47:37 -0700

    Updated logic for AM/PM and calling AM/PM directly based on time of day; also updated styling for responsive design

--- projects/digital_clock/script.js
+++ projects/digital_clock/script.js
@@ -1,6 +1,5 @@
 const localTime = document.getElementById("time");
-const amTime = document.getElementById("am");
-const pmTime = document.getElementById("pm");
+const amPM = document.getElementById("ante-post");
 const localTimeZone = document.getElementById("localTZ");
 
 function getTime() {
@@ -8,27 +7,24 @@ function getTime() {
   let localTZ = date.match(/\([a-zA-Z\s]+/g)[0].slice(1);
   let time = date.match(/\d{2}:\d{2}:\d{2}/g)[0];
   let hours = time.slice(0, 2);
-  if (hours[0] === "0") {
-    localTime.innerHTML = time;
-    amTime.style.opacity = 1;
-    localTimeZone.innerHTML = localTZ;
-  } else {
-    if (Number(hours) < 12) {
-        localTime.innerHTML = time;
-        amTime.style.opacity = 1;
-        localTimeZone.innerHTML = localTZ;
-    } else if (Number(hours) == 12) {
-        localTime.innerHTML = time;
-        pmTime.style.opacity = 1;
-        localTimeZone.innerHTML = localTZ;
-    } else if (Number(hours) < 22) {
-        localTime.innerHTML = "0" + String(hours - 12) + time.slice(2);
-        pmTime.style.opacity = 1;
-        localTimeZone.innerHTML = localTZ;
+
+  localTimeZone.innerHTML = localTZ;
+
+  if (hours < 12 || hours == 24) {
+    amPM.innerHTML = "AM";
+    if (hours == 24) {
+      localTime.innerHTML = "00" + time.slice(2); 
     } else {
-        localTime.innerHTML = String(hours - 12) + time.slice(2);
-        pmTime.style.opacity = 1;
-        localTimeZone.innerHTML = localTZ;
+      localTime.innerHTML = time;
+    }
+  } else {
+    amPM.innerHTML = "PM"
+    if (hours == 12) {
+      localTime.innerHTML = time;
+    } else if (hours < 22) {
+      localTime.innerHTML = "0" + String(hours - 12) + time.slice(2);
+    } else {
+      localTime.innerHTML = String(hours - 12) + time.slice(2);
     }
   }
 }
