8051f3e70ac5c1b6accefcdd8f4aa5ab7e774580
Ka Hung Lee <lee.kahung414@gmail.com>
2022-08-31 23:47:52 -0700

    Updating font sizes using clamp for responsive design

--- projects/digital_clock/script.js
+++ projects/digital_clock/script.js
@@ -1,7 +1,7 @@
 const localTime = document.getElementById("time");
 const amTime = document.getElementById("am");
 const pmTime = document.getElementById("pm");
-const localTZ = document.getElementById("localTZ");
+const localTimeZone = document.getElementById("localTZ");
 
 function getTime() {
   let date = Date();
@@ -9,26 +9,26 @@ function getTime() {
   let time = date.match(/\d{2}:\d{2}:\d{2}/g)[0];
   let hours = time.slice(0, 2);
   if (hours[0] === "0") {
-      localTime.innerHTML = time;
-      amTime.style.opacity = 1;
-      localTZ.innerHTML = localTZ;
+    localTime.innerHTML = time;
+    amTime.style.opacity = 1;
+    localTimeZone.innerHTML = localTZ;
   } else {
     if (Number(hours) < 12) {
         localTime.innerHTML = time;
         amTime.style.opacity = 1;
-        localTZ.innerHTML = localTZ;
+        localTimeZone.innerHTML = localTZ;
     } else if (Number(hours) == 12) {
         localTime.innerHTML = time;
         pmTime.style.opacity = 1;
-        localTZ.innerHTML = localTZ;
+        localTimeZone.innerHTML = localTZ;
     } else if (Number(hours) < 22) {
         localTime.innerHTML = "0" + String(hours - 12) + time.slice(2);
         pmTime.style.opacity = 1;
-        localTZ.innerHTML = localTZ;
+        localTimeZone.innerHTML = localTZ;
     } else {
         localTime.innerHTML = String(hours - 12) + time.slice(2);
         pmTime.style.opacity = 1;
-        localTZ.innerHTML = localTZ;
+        localTimeZone.innerHTML = localTZ;
     }
   }
 }
