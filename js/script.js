a5633bc958e69b009412996dd84ce8811ef8d826
Ka Hung Lee <lee.kahung414@gmail.com>
2022-09-15 07:42:35 -0700

    Swapped original blur animation with scrolling blur using Intersection Observer API and CSS and simplified function for changing theme colors on main site on script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,55 @@
+/* Function to change theme color */
 const darkMode = {
   backgroundColor: "rgb(40, 40, 40)",
   fontColor: "rgb(240, 240, 240)",
   navbarColor: "rgb(240, 240, 240)"
-}
+};
 
 const lightMode = {
   backgroundColor: "rgba(0, 127, 255, 0.4)",
   fontColor: "black",
   navbarColor: "orange"
-}
+};
+
+const rootVariables = ["--animation-border-color-end", "--color-back", "--color-sec", "--color-font"];
+const colorOrder = ["fontColor", "backgroundColor", "navbarColor", "fontColor"];
+
+const getTheme = (theme) => {
+  for (let i = 0; i < rootVariables.length; i++) {
+    document.documentElement.style.setProperty(rootVariables[i], theme[colorOrder[i]]);
+  }
+};
 
 function darkLightMode() {
   if (document.getElementById("toggle").checked == false) {
     document.querySelector(".nav-bar").style.backgroundColor = darkMode["navbarColor"];
     document.querySelector(".github-icon").style.filter = "brightness(100)";
-    document.documentElement.style.setProperty("--animation-border-color-end", darkMode["fontColor"]);
-    document.documentElement.style.setProperty("--color-back", darkMode["backgroundColor"]);
-    document.documentElement.style.setProperty("--color-sec", darkMode["navbarColor"]);
-    document.documentElement.style.setProperty("--color-font", darkMode["fontColor"]);
+    document.querySelector(".self-photo").style.opacity = 0.7;
+    getTheme(darkMode);
   } else {
     document.querySelector(".nav-bar").style.backgroundColor = lightMode["navbarColor"];
     document.querySelector(".github-icon").style.filter = "brightness(0)";
-    document.documentElement.style.setProperty("--animation-border-color-end", lightMode["fontColor"]);
-    document.documentElement.style.setProperty("--color-back", lightMode["backgroundColor"]);
-    document.documentElement.style.setProperty("--color-sec", lightMode["navbarColor"]);
-    document.documentElement.style.setProperty("--color-font", lightMode["fontColor"]);
+    document.querySelector(".self-photo").style.opacity = 1;
+    getTheme(lightMode);
   }
 }
+
+/* Function to deal with scrolling animation */
+const hiddenSections = document.querySelectorAll(".hidden");
+
+const sectObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    root: document.body.main,
+    threshold: "0.50",
+  }
+);
+
+hiddenSections.forEach((sect) => sectObserver.observe(sect));
