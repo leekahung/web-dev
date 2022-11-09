fef381ebaec0e6d9f1a954f8880c3e072d14af5f
Ka Hung Lee <lee.kahung414@gmail.com>
2022-11-08 19:58:42 -0800

    Updating HTML/SASS/JS based on BEM convention

--- projects/js_calculator/src/script.js
+++ projects/js_calculator/src/script.js
@@ -16,9 +16,9 @@ const calcInputs = new Map([
   [20, "ans"], [21, "0"], [22, "."], [23, "CE"], [24, "="]
 ]);
 
-const calcButtons = document.getElementById("calc-buttons");
-const numInput = document.getElementById("calc-input");
-const numOutput = document.getElementById("calc-output");
+const calcButtons = document.getElementById("calc-grp__buttons");
+const numInput = document.getElementById("calc-grp__input");
+const numOutput = document.getElementById("calc-grp__output");
 
 /* Main Grid for Calculator Buttons */
 const makeGrid = (rows, cols) => {
@@ -27,7 +27,7 @@ const makeGrid = (rows, cols) => {
   for (let i = 0; i < rows * cols; i++) {
     const buttons = document.createElement("div");
     buttons.innerText = calcInputs.get(i);
-    calcButtons.appendChild(buttons).className = "grid-buttons";
+    calcButtons.appendChild(buttons).className = "calc-grp__grid-buttons";
     calcButtons.appendChild(buttons).style.cursor = "pointer";
     switch (calcInputs.get(i)) {
       case "CE":
@@ -236,7 +236,7 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-const moveCalcBtn = document.getElementById("move-calc-btn");
+const moveCalcBtn = document.getElementById("bottom-grp__move-calc-btn");
 
 moveCalcBtn.addEventListener("click", () => {
   if (moveCalcBtn.innerText === "Move Calculator Left") {
