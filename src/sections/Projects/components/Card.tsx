1f8d9d1134c9ee8de6c41d05c1d19bac23893c5d
Ka Hung Lee <lee.kahung414@gmail.com>
2025-05-29 16:12:34 -0700

    Update project cards as links to website

--- src/sections/Projects/components/Card.tsx
+++ src/sections/Projects/components/Card.tsx
@@ -1,5 +1,4 @@
 import ExternalLink from "@/shared/components/ExternalLink";
-import LinkIcon from "@/shared/components/icons/LinkIcon";
 import StackIcon from "@/shared/components/icons/StackIcon";
 
 interface Props {
@@ -16,23 +15,22 @@ export default function Card({
   repoLink,
 }: Props) {
   return (
-    <div className="flex flex-none p-4 border rounded-lg items-center flex-col gap-2 w-[250px] bg-slate-200/80 dark:bg-slate-800/80 z-20">
+    <a
+      href={siteLink}
+      target="_blank"
+      className="flex flex-none p-4 border rounded-lg items-center flex-col gap-2 w-[250px] bg-slate-200/80 dark:bg-slate-800/80 z-20 hover:bg-slate-300/80 hover:dark:bg-slate-700/80 duration-700"
+    >
       <h3 className="text-2xl flex-1 items-center flex justify-center h-10">
         {title}
       </h3>
       <p className="flex items-center h-[72px]">{description}</p>
       <div className="flex gap-2 justify-center">
-        <ExternalLink to={siteLink}>
-          <div className="h-8 w-8 border rounded-xl p-1 md:hover:text-blue-500 md:dark:hover:text-orange-300">
-            <LinkIcon />
-          </div>
-        </ExternalLink>
         <ExternalLink to={repoLink}>
           <div className="h-8 w-8 border rounded-xl p-1 md:hover:text-blue-500 md:dark:hover:text-orange-300">
             <StackIcon />
           </div>
         </ExternalLink>
       </div>
-    </div>
+    </a>
   );
 }
--- src/shared/components/icons/LinkIcon.tsx
+++
@@ -1,7 +0,0 @@
-import SVGIcon from "../SVGIcon";
-
-export default function LinkIcon() {
-  return (
-    <SVGIcon svgPathD="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
-  );
-}
